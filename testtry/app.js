9d5ec31059aba823f22b35b01e98324883553c7c
Pooria Rezaee <pooriarezaee.dev@gmail.com>
2026-06-29 12:50:05 +0330

    dw

--- testtry/app.js
+++ testtry/app.js
@@ -21,3 +21,41 @@ span.onclick = function para(e) {
 
 
 }
+
+
+
+function pori(x) {
+    return x * 10
+}
+
+
+
+function sum(x, y) {
+    return x * y
+}
+
+
+const pooria = () => {
+
+    console.log("dwdqw");
+
+
+}
+
+const h1 = document.getElementById("h1")
+
+
+
+h1.innerHTML = 
+function para1(x, y) {
+
+    return x * y
+
+}
+
+function para2(home) {
+
+
+    para1(10, 20)
+
+}
